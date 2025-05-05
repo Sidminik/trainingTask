60bd490fc4f7a1fb3520c2f81b4c4913a3287d12
Sidminik <okb.dns@gmail.com>
2025-05-05 16:33:52 +0700

    16:33

--- user-services/src/main/java/co/avbinvest/userservices/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package co.avbinvest.userservices.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(Long id) {
-        super("User " + id + " not found");
-    }
-}
--- user-services/src/main/java/co/avbinvest/userservices/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package co.avbinvest.userservices.repository;
-
-import co.avbinvest.userservices.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
