c19945b86165d5080ff78b263fa20c79aa03e1d6
Sidminik <okb.dns@gmail.com>
2025-05-05 16:16:33 +0700

    16:16

--- company-services/src/main/java/co/avbinvest/companyservices/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package co.avbinvest.companyservices.repository;
-
-import co.avbinvest.companyservices.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
