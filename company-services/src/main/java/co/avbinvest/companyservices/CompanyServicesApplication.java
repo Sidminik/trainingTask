22b923e2a5c0ac5b29e521637e11055e92e197fc
Sidminik <okb.dns@gmail.com>
2025-05-15 15:00:15 +0700

    15.05.2025 15:00

---
+++ company-services/src/main/java/co/avbinvest/companyservices/CompanyServicesApplication.java
@@ -0,0 +1,13 @@
+package co.avbinvest.companyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyServicesApplication.class, args);
+	}
+
+}
